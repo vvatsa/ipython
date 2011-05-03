d59b8aeb7227bf6483afc20d6893e031b8e5ef44
Brian Granger <ellisonbg@gmail.com>
2011-05-02 22:20:27 -0700

    Initial latex printing for sympy and fixes to autogrow.

--- setupbase.py
+++ setupbase.py
@@ -271,6 +271,7 @@ def find_scripts(entry_points=False):
     if entry_points:
         console_scripts = [
             'ipython = IPython.frontend.terminal.ipapp:launch_new_instance',
+            'ipython-notebook = IPython.frontend.html.notebook.notebook:main',
             'pycolor = IPython.utils.PyColorize:main',
             'ipcontroller = IPython.parallel.apps.ipcontrollerapp:launch_new_instance',
             'ipengine = IPython.parallel.apps.ipengineapp:launch_new_instance',
@@ -293,6 +294,7 @@ def find_scripts(entry_points=False):
                    pjoin(parallel_scripts, 'iplogger'),
                    pjoin(main_scripts, 'ipython'),
                    pjoin(main_scripts, 'ipython-qtconsole'),
+                   pjoin(main_scripts, 'ipython-notebook'),
                    pjoin(main_scripts, 'pycolor'),
                    pjoin(main_scripts, 'irunner'),
                    pjoin(main_scripts, 'iptest')
